32d6cdce2984b1741a5380741ae240604f2ff5eb
unknown <LGCNS@DESKTOP-GMAJF29.lgcns.com>
2022-07-08 13:43:28 +0900

    new ProfileController 3

--- src/main/java/org/springframework/samples/petclinic/system/ProfileController.java
+++ src/main/java/org/springframework/samples/petclinic/system/ProfileController.java
@@ -20,6 +20,7 @@ package org.springframework.samples.petclinic.system;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Arrays;
 import java.util.List;
@@ -28,7 +29,7 @@ import java.util.List;
 @RestController
 public class ProfileController {
 	
-	@Resource
+	@Autowired
     private final Environment env;
 
     @GetMapping("/profile")
