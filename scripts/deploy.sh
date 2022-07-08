d7790268c235f7a7c05f0375eaca64e5de690a55
unknown <LGCNS@DESKTOP-GMAJF29.lgcns.com>
2022-07-08 10:27:30 +0900

    deploy sh 9 test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -37,3 +37,5 @@ echo "> $JAR_NAME 실행"
 #nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 #nohup java -jar $REPOSITORY/spring-petclinic-2.6.0.jar  > $REPOSITORY/nohup.out 2>&1 &
 nohup java -jar $REPOSITORY/spring-petclinic-2.6.0.jar 
+
+#[stderr]ERROR in ch.qos.logback.core.rolling.RollingFileAppender[rollingFile] - Failed to create parent directories for [/./logs/application.log]
